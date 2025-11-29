a216a5260d443f4d422bd56f3417c20454e5d6ae
sondoannam <sondoannam202@gmail.com>
2025-11-29 12:35:33 +0700

    feat: :sparkles: correct prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
     super({ adapter });
+  }
+  async onModuleInit() {
+    // Note: this is optional
+    await this.$connect();
   }
 }
